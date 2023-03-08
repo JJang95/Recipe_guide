5a8d0e6686024504c47c2c472546eab4cf096159
JJang95 <jojang1994@gmail.com>
2023-03-07 21:25:40 -0500

    Finished Category component

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -1,8 +1,10 @@
 import Pages from "./pages/Pages";
+import Categoy from "./components/Categoy";
 
 function App() {
   return (
     <div className="App">
+      <Categoy />
       <Pages />
     </div>
   );
--- recipe/src/components/Categoy.jsx
+++ recipe/src/components/Categoy.jsx
@@ -1,10 +1,31 @@
 import {FaPizzaSlice, FaHamburger} from 'react-icons/fa';
-import {GiNoodles, GiChopSticks} from 'react-icons/gi'
+import {GiNoodles, GiChopsticks} from 'react-icons/gi'
 import styled from "styled-components";
+import {NavLink} from 'react-router-dom';
 
 function Categoy() {
     return (
-        <div>Categoy</div>
+        <div>
+            <div>
+                <FaPizzaSlice/>
+                <h4>Italian</h4>
+            </div>
+
+            <div>
+                <FaHamburger/>
+                <h4>American</h4>
+            </div>
+
+            <div>
+                <GiNoodles/>
+                <h4>Thai</h4>
+            </div>
+
+            <div>
+                <GiChopsticks/>
+                <h4>Japanese</h4>
+            </div>
+        </div>
     )
 }
 
