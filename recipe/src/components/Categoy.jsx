84cd9d5d8dd188de1fd6ce45bd668e46b0a99f65
JJang95 <jojang1994@gmail.com>
2023-03-07 21:21:16 -0500

    Starting Category jsx component

---
+++ recipe/src/components/Categoy.jsx
@@ -0,0 +1,11 @@
+import {FaPizzaSlice, FaHamburger} from 'react-icons/fa';
+import {GiNoodles, GiChopSticks} from 'react-icons/gi'
+import styled from "styled-components";
+
+function Categoy() {
+    return (
+        <div>Categoy</div>
+    )
+}
+
+export default Categoy
