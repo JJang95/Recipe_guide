5ef311b2d0d0ae775e8f570569f2f19bc3c51f76
JJang95 <jojang1994@gmail.com>
2023-03-10 02:16:25 -0500

    Finished setting up recipe with api and put in onClick functionality for buttons

--- recipe/src/pages/Recipe.jsx
+++ recipe/src/pages/Recipe.jsx
@@ -8,6 +8,7 @@ function Recipe() {
 
     let params = useParams();
     const [details, setDetails] = useState({});
+    const [activeTab, setActiveTab] = useState('instructions');
 
     const fetchDetails = async () => {
         const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`);
@@ -21,8 +22,53 @@ function Recipe() {
     }, [params.name]);
 
     return (
-        <div>{details.title}</div>
+        <DetailWrapper>
+            <div>
+                <h2>{details.title}</h2>
+                <img src={details.image} alt="" />
+            </div>
+            <Info>
+                <Button className = {activeTab === 'instructions' ? 'active' : ''} onClick={() => setActiveTab('instructions')}>Instructions</Button>
+                <Button className = {activeTab === 'ingredients' ? 'active' : ''} onClick={() => setActiveTab('ingredients')}>Ingredients</Button>
+            </Info>
+        </DetailWrapper>
     )
 }
+
+const DetailWrapper = styled.div `
+    margin-top: 10rem;
+    margin-bottom: 5rem;
+    display: flex;
+
+    .active {
+        background: linear-gradient(35deg, #494949, #313131);
+        color: white;
+    }
+
+    h2 {
+        marin-bottom: 2rem;
+    }
+
+    li {
+        font-size: 1.2rem;
+        line-height: 2.5rem;
+    }
+
+    ul {
+        margin-top: 2rem;
+    }
+`
+
+const Button = styled.button `
+    padding: 1rem 2rem;
+    color: #313131;
+    border: 2px solid black;
+    margin-right: 2rem;
+    font-weight: 600;
+`
+
+const Info = styled.div `
+    margin-left: 10rem;
+`
 
 export default Recipe
