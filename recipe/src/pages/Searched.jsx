390078d4d6596010e2422d0b4e505d2d04dccf50
JJang95 <jojang1994@gmail.com>
2023-03-09 23:31:52 -0500

    Set up searched with search

---
+++ recipe/src/pages/Searched.jsx
@@ -0,0 +1,11 @@
+import Home from './Home'
+import Cuisine from './Cuisine'
+import {Route, Routes} from 'react-router-dom'
+
+function Searched() {
+    return (
+        <div>Searched</div>
+    )
+}
+
+export default Searched
